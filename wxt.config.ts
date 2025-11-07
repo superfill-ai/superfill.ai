97a02c446d3c82266643adc501ca9ad1a06331f3
Mihir Kumar <mihirpandey.13@gmail.com>
2025-11-07 12:52:41 +0530

    fix: update icon sizes in wxt.config.ts

--- wxt.config.ts
+++ wxt.config.ts
@@ -20,10 +20,11 @@ export default defineConfig({
     manifest: {
       icons: {
         16: "/icon-16.png",
-        24: "/icon-24.png",
+        32: "/icon-32.png",
         48: "/icon-48.png",
-        96: "/icon-96.png",
         128: "/icon-128.png",
+        256: "/icon-256.png",
+        512: "/icon-512.png",
       },
     },
   },
