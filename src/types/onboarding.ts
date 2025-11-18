1d775961979b05e03ce6d18c7325d253bee8221e
Mihir Kumar <mihirpandey.13@gmail.com>
2025-11-18 13:30:42 +0530

    refactor: remove unused OnboardingFormData interface

--- src/types/onboarding.ts
+++
@@ -1,9 +0,0 @@
-export interface OnboardingFormData {
-  firstName: string;
-  lastName: string;
-  personalEmail: string;
-  workEmail?: string;
-  phoneNumber: string;
-  address: string;
-  country: string;
-}
